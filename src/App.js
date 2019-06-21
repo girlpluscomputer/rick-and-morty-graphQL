d314cc4d74f9867b409dedb4d102bf84230b7cbc
Samantha  Nunez <samanthan-c@herbalife.com>
2019-06-21 10:36:47 -0500

    Bringing data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Button from "./components/Button";
 import Character from "./components/Character";
-import image from "./img/rick-and-morty.png";
+import logo from "./img/rick-and-morty.png";
 
 import "./App.css";
 
@@ -9,17 +9,55 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: {}
+      nextPage: 1,
+      loading: true,
+      error: null,
+      data: {
+        results: []
+      }
     };
   }
+  componentDidMount() {
+    this.fetchCharacters();
+  }
 
+  fetchCharacters = async () => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${this.state.nextPage}`
+      );
+      const data = await response.json();
+      this.setState({
+        data: {
+          info: data.info,
+          results: [].concat(this.state.data.results, data.results)
+        },
+        loading: false,
+        nextPage: this.state.nextPage + 1
+      });
+      // console.log(data);
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error
+      });
+      console.log(error);
+    }
+  };
+
+  handleclick = e => {
+    console.log("Clicked");
+    // console.log(this.state.data.results.length);
+  };
   render() {
     return (
       <div className="App">
-        <img src={image} alt="h" width="200" />
+        <img src={logo} alt="logo" width="250" />
         <h2 className="title">Character generator</h2>
         <Character />
-        <Button />
+        <Button handleclick={this.handleclick} />
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
-import "./Button.css";
+// import "./Button.css";
+import styled from "styled-components";
 
-function Button() {
-  return <button className="button">Generate</button>;
+const RoundButton = styled.button`
+  border: none;
+  height: 36px;
+  padding: 0 16px;
+  border-radius: 16px;
+  background-color: #5fc471;
+  color: azure;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+function Button({ handleclick }) {
+  return <RoundButton onClick={handleclick}>Generate</RoundButton>;
 }
 export default Button;
