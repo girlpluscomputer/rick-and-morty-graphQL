418efbb9c73a979a7699df77e29893fe9b3aa9d0
Arath Jimenez <arathjz@Araths-MacBook-Pro.local>
2019-07-03 21:44:21 -0500

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import FindCharacter from "./components/findcharacter";
-import FindLocation from "./components/findlocation";
-import RandomCharacter from "./components/randomcharacter";
-import RandomLocation from "./components/randomlocation";
-import Navbar from "./components/common/navbar";
-import Header from "./elements";
-import Home from "./components/home";
+import React, { Fragment } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import FindCharacter from './components/findcharacter';
+import FindLocation from './components/findlocation';
+import RandomCharacter from './components/randomcharacter';
+import RandomLocation from './components/randomlocation';
+import Navbar from './components/common/navbar';
+import Header from './elements';
+import Home from './components/home';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <div>
-          <Header>
-            <Navbar />
-          </Header>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/find-character" component={FindCharacter} />
-            <Route path="/find-location" component={FindLocation} />
-            <Route path="/random-character" component={RandomCharacter} />
-            <Route path="/random-location" component={RandomLocation} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <Fragment>
+      <Header>
+        <Navbar />
+      </Header>
+      <Switch>
+        <Route path="/find-character" component={FindCharacter} />
+        <Route path="/find-location" component={FindLocation} />
+        <Route path="/random-character" component={RandomCharacter} />
+        <Route path="/random-location" component={RandomLocation} />
+        <Redirect to="/find-character" />
+      </Switch>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { ApolloProvider } from "react-apollo";
-import client from "./apollo";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import client from './apollo';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
