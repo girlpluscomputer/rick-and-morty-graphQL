97f1319d13477c3a9e3befc870fa193a9e163531
Samantha  Nunez <samanthan-c@herbalife.com>
2019-07-21 15:48:16 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Fragment } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { withApollo } from "react-apollo";
 
+import FindCharacter from "./components/views/findcharacter";
 import FindLocation from "./components/views/findlocation";
 import RandomCharacter from "./components/views/randomcharacter";
 import RandomLocation from "./components/views/randomlocation";
 import Navbar from "./components/common/navbar";
+import Header from "./elements";
+
+function App(props) {
+  const { client } = props;
+
+  return (
     <Fragment>
       <Header>
         <Navbar />
